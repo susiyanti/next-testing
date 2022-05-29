cf175d1a00099d016628b0f9491214d7cc892502
susiyanti <susiyanti.g11@gmail.com>
2022-05-29 08:14:01 +0000

    [skip ci] Updates

---
+++ _next/static/IwXZH9jpNPcD-l7SR5QXP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F04b035e6b090a5f1b9a1.css","static\u002Fchunks\u002Fpages\u002Findex-38f12f44a763174aac9d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
